7d086610d78209366cf0d09316a9d23d92b7aa26
Iniyealakeret2 <iniyealakeret1@gmail.com>
2022-05-26 22:13:47 +0100

    tweets with date

--- src/main/kotlin/section3_BIG_O/FindTweet.kt
+++ src/main/kotlin/section3_BIG_O/FindTweet.kt
@@ -1,11 +1,15 @@
 package section3_BIG_O
 
-fun main(args: Array<String>){
-    val arrayTweets = listOf("hi", "emma", "solo")
+fun main(){
+    val arrayTweets = listOf(FindTweet("hi",2021), FindTweet("emma",2022), FindTweet("hi",2020))
     findLatestAndOldestTweet(arrayTweets)
 }
-fun findLatestAndOldestTweet(tweets: List<String>){
+fun findLatestAndOldestTweet(tweets: List<FindTweet>){
     val latestTweet = tweets[0]
     val oldestTweet = tweets[tweets.size - 1]
     println("$latestTweet, $oldestTweet")
 }
+data class FindTweet(
+    val tweet: String,
+    val date: Int
+)
