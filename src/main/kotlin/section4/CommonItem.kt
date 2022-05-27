eacd292df5745fd58a96120de6fbc6b3179a1230
Iniyealakeret2 <iniyealakeret1@gmail.com>
2022-05-27 18:07:12 +0100

    optimized common items array using forEach{} with HashSet()

--- src/main/kotlin/section4/CommonItem.kt
+++ src/main/kotlin/section4/CommonItem.kt
@@ -4,14 +4,14 @@ fun main(){
     val array1 = listOf('a','b','c')
     val array2 = listOf('d','e','f')
     val array3 = listOf('h','i','c')
-    println("BRUTE FORCE")
-    println("---------------------")
-    println(commonItemsInTwoArrays(array1,array2))
-    println(commonItemsInTwoArrays(array1,array3))
+//    println("BRUTE FORCE")
+//    println("---------------------")
+//    println(commonItemsInTwoArrays(array1,array2))
+//    println(commonItemsInTwoArrays(array1,array3))
     println("---------------------")
     println("OPTIMIZED WITH HASHSET")
-    println(commonItemsInTwoArraysOptimal(array1,array2))
-    println(commonItemsInTwoArraysOptimal(array1,array3))
+    println(commonItemsInTwoArraysOptimalForEach(array1,array2))
+    println(commonItemsInTwoArraysOptimalForEach(array1,array3))
 }
 // QUESTION: Given two arrays create a function that let user know(true/false)
 // whether these two arrays contain any common items
@@ -23,6 +23,9 @@ fun main(){
 //SOLUTION STEPS( from asking questions)
 // we have two parameters -> arrays -> no size limit
 // we return true or false
+
+//QUESTION TO ASK
+// can we always assume two input parameters, and will they always be arrays?
 
 //Brute force solutions
 fun commonItemsInTwoArrays(array1: List<Char>, array2: List<Char>): Boolean{
@@ -37,6 +40,8 @@ fun commonItemsInTwoArrays(array1: List<Char>, array2: List<Char>): Boolean{
 
 // optimal solutions
 fun commonItemsInTwoArraysOptimal(array1: List<Char>, array2: List<Char>): Boolean{
+    //Checking if an input is not given(edge case)
+    if (array1.isEmpty() || array2.isEmpty()) return false
     // create an hashSet
     val hashSet = HashSet<Char>()
     for (i in array1){
@@ -48,3 +53,12 @@ fun commonItemsInTwoArraysOptimal(array1: List<Char>, array2: List<Char>): Boole
     }
     return false
 }// Time complexity is O(a + b) => O(n)
+
+// Using a foreach to solve the problem
+fun commonItemsInTwoArraysOptimalForEach(array1: List<Char>, array2: List<Char>): Boolean{
+    if (array1.isEmpty() || array2.isEmpty()) return false
+    val hashSet = HashSet<Char>()
+    array1.forEach { hashSet.add(it) }
+    array2.forEach { if(hashSet.contains(it)) return true}
+    return false
+}
