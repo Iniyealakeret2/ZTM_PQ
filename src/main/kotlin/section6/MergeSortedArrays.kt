2797bcb84611d8651cbda9efb95efc5f9899c54c
Iniyealakeret2 <iniyealakeret1@gmail.com>
2022-06-14 11:15:58 +0100

    first Linked List creation

--- src/main/kotlin/section6/MergeSortedArrays.kt
+++ src/main/kotlin/section6/MergeSortedArrays.kt
@@ -1,10 +1,25 @@
 package section6
 
+@OptIn(ExperimentalUnsignedTypes::class)
 fun main(){
     val array1 = listOf(1,2,3,4)
-    val array2 = listOf<Int>()
-    println(mergerSortedArrays(array1, array2))
+    val array2 = listOf(0,0,0)
 
+    val nums1 = IntArray(6)
+    nums1[0] = 1
+    nums1[1] = 2
+    nums1[2] = 3
+    nums1[3] = 0
+    nums1[4] = 0
+    nums1[5] = 0
+    val m = 3
+    val nums2 = IntArray(3)
+    nums2[0] = 1
+    nums2[1] = 2
+    nums2[2] = 3
+    val n = 3
+    println(mergerSortedArrays(array1, array2))
+    println(mergerSortedArrays2(nums1,m,nums2,n))
 
 }
 // QUESTION: Given two arrays that are sorted, can you merge these two arrays into one?
@@ -14,7 +29,35 @@ private fun mergerSortedArrays(array1: List<Int>, array2: List<Int>): List<Int> 
 
     if (array1.isEmpty()) return array2
     if (array2.isEmpty()) return array1
+
     mergedArray.addAll(array1)
     mergedArray.addAll(array2)
     return mergedArray
+}
+
+// LEETCODE QUESTION(You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+// and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+@OptIn(ExperimentalUnsignedTypes::class)
+fun mergerSortedArrays2(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
+
+    var pointer = n + m - 1 // pointing to the end of nums1, which is n+m-1
+    var pointer1 = m - 1 // pointing to the last element of nums1 array
+    var pointer2 = n - 1 // pointing to the last element of nums2 array
+    while (pointer1>=0 && pointer2>=0) {
+        if (nums1[pointer1] > nums1[pointer2]){
+            nums1[pointer] = nums1[pointer1]
+            pointer1--
+            pointer--
+        }else{
+            nums1[pointer] = nums2[pointer2]
+            pointer2--
+            pointer--
+        }
+    }
+    while (pointer2 >=0){
+        nums1[pointer] = nums2[pointer2]
+        pointer2--
+        pointer--
+    }
 }
--- src/main/kotlin/section7/FirstReocuuringCharacter.kt
+++ src/main/kotlin/section7/FirstReocuuringCharacter.kt
@@ -2,11 +2,14 @@ package section7
 
 //QUESTION: Given an array tell me the first reoccurring character
 fun main(){
-    println(firstReoccurringCharacter(listOf(1,2,3,4,5,6,7,8)))
-    println(hashTableFirstReoccurringCharacter(listOf(1,2,3,4,5,6,7,8,2)))
+//    println(firstReoccurringCharacter(listOf(1,2,3,4,5,6,7,8)))
+//    println(hashTableFirstReoccurringCharacter(listOf(1,2,3,4,5,6,7,8,2)))
+    println(test(listOf(1,0,0,2,2)))
 }
+
+// first solution
 fun firstReoccurringCharacter(array: List<Int>): Int{
-    if(array.isEmpty()) return 0
+    if(array.isEmpty() || array.size < 2) return 0
     for (i in array.indices) {
         for (j in i + 1 until array.size) {
             if (array[i] == array[j]) {
@@ -17,6 +20,7 @@ fun firstReoccurringCharacter(array: List<Int>): Int{
     return 0
 }
 
+//second best solution
   fun hashTableFirstReoccurringCharacter(array: List<Int>): Int {
       val hashSet = HashSet<Int>()
       if (array.isEmpty()) return 0
@@ -28,3 +32,17 @@ fun firstReoccurringCharacter(array: List<Int>): Int{
       }
       return 0
   }
+
+// best solution
+fun test(array: List<Int>): Int{
+    val hashSet = HashSet<Int>()
+    if(array.isEmpty()) return 0
+    for (i in array.indices){
+        if(hashSet.contains(array[i])){
+            return array[i]
+        }else{
+            hashSet.add(array[i])
+        }
+    }
+    return 0
+}
